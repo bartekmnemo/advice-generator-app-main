aad8a3d4f5a5400444e204bb3fc9c75a567bd99b
St33v <48390535+bartekmnemo@users.noreply.github.com>
2022-04-23 23:17:35 +0200

    fix advice text section

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const btn = document.getElementById("btn");
 const advNrId = document.getElementById("advNrId");
 const advTextId = document.getElementById("advTextId");
 
+var text = document.createTextNode("This just got added");
+
 btn.addEventListener("click", (e) => {
-  console.log(advTextId);
+  advTextId.appendChild(text);
 });
