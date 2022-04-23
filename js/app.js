2d59e273fac11b698ecf1dae5add4fc656da58fa
St33v <48390535+bartekmnemo@users.noreply.github.com>
2022-04-23 23:55:49 +0200

    add fetch api btn and functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 const btn = document.getElementById("btn");
 const advNrId = document.getElementById("advNrId");
-const advTextId = document.getElementById("advTextId");
+const advTxtId = document.getElementById("advTxtId");
+let nrAdvice = "";
 
 var text = document.createTextNode("This just got added");
 
-btn.addEventListener("click", (e) => {
-  advTextId.appendChild(text);
-});
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then(function (response) {
+      // The API call was successful!
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(function (data) {
+      advNrId.textContent = data.slip.id;
+      advTxtId.textContent = data.slip.advice;
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
+}
+
+btn.addEventListener("click", getAdvice);
+
+getAdvice();
