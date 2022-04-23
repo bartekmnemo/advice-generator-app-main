9dfd150a949f9d2070f85cf580f8cbfcc091992d
St33v <48390535+bartekmnemo@users.noreply.github.com>
2022-04-23 23:08:31 +0200

    do styles

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+const advNrId = document.getElementById("advNrId");
+const advTextId = document.getElementById("advTextId");
+
+btn.addEventListener("click", (e) => {
+  console.log(advTextId);
+});
